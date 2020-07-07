8248bb6282fc1ac2f1c93812974ae959bcc0e09d
hennam <mostafa.henna@emc.com>
2020-07-07 15:29:05 +0200

    added user model with jwt authentication

---
+++ User/UserModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: validator
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    passwordResetToken: {
+        type: String,
+    },
+});
+
+
+function validator(v) {
+    return /^[^@\s]+@(dell.com)|(emc.com)$/.test(v);
+};
+
+UserSchema.plugin(beautifyUnique)
+module.export = mongoose.model('User', UserSchema).init()
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    secret: 'techdev',
+    resetSecret: 'TechD3V',
+    hostUrl: 'http://localhost:3300',
+    publicRoutes: [
+        '/user/register', '/user/login'
+    ]
+};
