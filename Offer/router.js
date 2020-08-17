09a46fb030f3aaa5e1040dbe85965b63552f9bd6
hennam <mostafa.henna@emc.com>
2020-08-17 14:39:07 +0200

    modified endpoints to match new offers views

--- Offer/router.js
+++ Offer/router.js
@@ -8,5 +8,7 @@ router.route('/addOffer').post(controller.addOffer);
 router.route('/getOffer/:name').get(controller.getOffer);
 router.route('/test').post(controller.testUpload);
 router.route('/test').get(controller.testRetrieve);
+router.route('/getByCategory/:name').get(controller.getOffersByCategory);
+router.route('/').get(controller.getOffers);
 
 module.exports = router
