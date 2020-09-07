ec656e47a2438b1ff07273eb62cb22143572fd30
v1ll41n <kareem.m.selim@gmail.com>
2020-09-07 10:53:42 +0200

    Fetch events by ERG

--- Event/controller.js
+++ Event/controller.js
@@ -39,5 +39,34 @@ module.exports = {
             else
                 res.status(500).send(`Unexpected error occured: ${err}`);
         }
-    }
+    },
+
+    getEventsByERG: async (req, res) => {
+        try {
+            let events = await Service.findByERG(req.params.erg);
+            res.status(200).send(events);
+        } catch (err) {
+            if (err.status)
+                res.status(err.status).send(err.message);
+            else
+                res.status(500).send(`Unexpected error occured: ${err}`);
+        }
+    },
+
+    getEvent: async (req, res) => {
+        try {
+            if (!req.params.name)
+                res.status(400).send({ message: "Missing parameter!" })
+
+            let event = await Service.findByName(req.params.name);
+            console.log(event);
+            let poster = await Service.retrievePoster(event.poster);
+            res.status(200).send({ event: event, poster: poster });
+        } catch (err) {
+            if (err.status)
+                res.status(err.status).send(err.message);
+            else
+                res.status(500).send(`Unexpected error occured: ${err}`);
+        }
+    },
 }
--- Event/router.js
+++ Event/router.js
@@ -4,7 +4,10 @@ const controller = require('./controller')
 
 router.use(express.json())
 
-router.route('').post(controller.addEvent);
-router.route('').get(controller.getEvents);
+router.route('/addEvent').post(controller.addEvent);
+router.route('/').get(controller.getEvents);
+router.route('/getEvent/:name').get(controller.getEvent);
+router.route('/getByERG/:erg').get(controller.getEventsByERG);
+
 
 module.exports = router
